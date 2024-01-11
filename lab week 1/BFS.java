927b9f2b5faaa3d2c177fc04ac2344ed20c162e4
dhruvi003 <cdhwani03@gmail.com>
2024-01-11 19:35:28 +0530

    Implemented BFS

--- lab week 1/BFS.java
+++ lab week 1/BFS.java
@@ -15,11 +15,36 @@ class Graph{
         boolean[] visited = new boolean[V];
         int[] queue = new int[V];
         
+        int head = 0, tail = 0;
+
+        visited[startpoint] = true;
+        queue[tail++] = startpoint;
+
+        while(head != tail){
+            int currentVertex = queue[head++];
+            System.out.print(currentVertex + " ");
+
+            for(int i =0; i< V; i++){
+                if(graph[currentVertex][i] == 1 && !visited[i]){
+                    visited[i] = true;
+                    queue[tail++] = i;
+                }
+            }
+        }
+
     }
 }
 
 public class BFS{
     public static void main(String[] args) {
+        
+        Graph g1 = new Graph(4);
+
+        g1.makeEdge(0,1);
+        g1.makeEdge(1,2);
+        g1.makeEdge(1,3);
+        System.out.println("Breadth First Traversal of given graph");
+        g1.implementBFS(0);
         
     }
 }
