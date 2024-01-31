1311f283718ca5466838a7a058c2420da1c1e89d
dhruvi003 <cdhwani03@gmail.com>
2024-01-31 15:56:47 +0530

    done some change in eight puzzle problem

--- lab week 3/EightPuzzle.java
+++ lab week 3/EightPuzzle.java
@@ -1,84 +1,127 @@
-import java.util.Arrays;
-import java.util.PriorityQueue;
+import java.util.*;
 
-// import org.w3c.dom.Node;
+class Node {
+
+    int[][] puzzle;
+    int x, y, cost, level;
+    Node parent;
+
+    // initialized the values
+    public Node(int[][] puzzle, int x, int y, int newX, int newY, int level, Node parent) {
+        this.puzzle = new int[3][3];
+        this.level = level;
+        this.parent = parent;
+        this.x = newX;
+        this.y = newY;
+        this.cost = Integer.MAX_VALUE;
+
+        // Copy puzzle state
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                // assign values 
+                this.puzzle[i][j] = puzzle[i][j];
+            }
+        }
+
+        // swapping the value of old node with the new values
+        int temp = this.puzzle[x][y];
+        this.puzzle[x][y] = this.puzzle[newX][newY];
+        this.puzzle[newX][newY] = temp;
+    }
+}
 
 public class EightPuzzle {
-    
     //creating the first node
     static Node root;
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         //initialise goal and start position by 2d array
-        int[][] initial = {{1,2,3},{5,6,0},{7,8,4}};
+        System.out.println("Enter initial state of puzzle:");
+        int[][] initial = new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                initial[i][j] = scanner.nextInt();
+            }
+        }
+        // int[][] initial = {{1,2,3},{5,6,0},{7,8,4}};
 
-        int[][] goal = {{1,2,3},{5,8,6},{0,7,4}};  // here 0 shows blank space
 
-        int x = 1 , y = 2; // x and y are coordinates of blank space for starting node
-        root = new Node(initial, x, y, x, 0, null, y);
-        StarSearch(root, goal);
+        // Get goal state
+        System.out.println("Enter goal state of puzzle:");
+        int[][] goal = new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                goal[i][j] = scanner.nextInt();
+            }
+        }
+        // int[][] goal = {{1,2,3},{5,8,6},{0,7,4}};  // here 0 shows blank space
 
+
+        // Get coordinates of the blank tile
+        System.out.println("Enter the x and y coordinates of the blank tile (0):");
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+
+        root = new Node(initial, x, y, x, y, 0, null);
+        AStarSearch(root, goal);
     }
 
-    public static void StarSearch(Node root, int[][] goal){
+    // A* Search Algorithm
+    public static void AStarSearch(Node root, int[][] goal) {
         //we need two list : OPEN and CLOSE
         // OPEN --> Store nodes that are yet to be explored
         // CLOSE --> store nodes that have been explored
 
         // first adding root node to open list
-        PriorityQueue<Node> OPEN = new PriorityQueue<>();
-        OPEN.add(root);
+        PriorityQueue<Node> openList = new PriorityQueue<>(Comparator.comparingInt(node -> node.cost));
+        openList.add(root);
 
-        // we traverse the until the open list is not empty, that means each and every node is explored
-        while(!OPEN.isEmpty()){
-            // creating a new node for each low value in open list
-            Node node = OPEN.poll();
+        while (!openList.isEmpty()) {
+            Node node = openList.poll();
 
-            //if current node is goal node
-            if(Arrays.deepEquals(node.puzzle, goal)){
+            // Goal Test
+            if (Arrays.deepEquals(node.puzzle, goal)) {
                 printPath(node);
                 return;
             }
 
-            //else we will generate all the steps
-            for(int[] direction : new int[][]{{1,0},{-1,0},{0,1},{0,-1}}){
+            // Generate all possible 
+            for (int[] direction : new int[][]{{1, 0}, {-1, 0}, {0, -1}, {0, 1}}) {
                 int newX = node.x + direction[0];
                 int newY = node.y + direction[1];
 
-                if(isValid(newX, newY)){
-                    Node child = new Node(node.puzzle, node.x, node.y, newX, node.level+1, node, newY);
+                if (isValid(newX, newY)) {
+                    Node child = new Node(node.puzzle, node.x, node.y, newX, newY, node.level + 1, node);
                     child.cost = calculateCost(child, goal);
-                    OPEN.add(child);
+                    openList.add(child);
                 }
             }
-
         }
     }
 
-    public static int calculateCost(Node child, int[][] goal) {
+    // Check if the new coordinates are valid
+    public static boolean isValid(int x, int y) {
+        return (x >= 0 && x < 3 && y >= 0 && y < 3);
+    }
+
+    // Calculate cost of a node
+    public static int calculateCost(Node node, int[][] goal) {
         int misplacedTiles = 0;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                if (child.puzzle[i][j] != 0 && child.puzzle[i][j] != goal[i][j]) {
+                if (node.puzzle[i][j] != 0 && node.puzzle[i][j] != goal[i][j]) {
                     misplacedTiles++;
                 }
             }
         }
-        return misplacedTiles + child.level;
+        return misplacedTiles + node.level;
     }
 
-
-
-    public static boolean isValid(int newX, int newY) {
-       if(newX >= 0 && newX < 3 && newY >=0 && newY <3){
-        return true;
-       }
-       return false;
-    }
-
+    // Print the path from root to goal
     public static void printPath(Node node) {
-        
-        if(node == null){
+        if (node == null) {
             return;
         }
         printPath(node.parent);
@@ -86,7 +129,8 @@ public class EightPuzzle {
         System.out.println();
     }
 
-    public static void printPuzzle(int[][] puzzle){
+    // Print the state of the puzzle
+    public static void printPuzzle(int[][] puzzle) {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 System.out.print(puzzle[i][j] + " ");
@@ -94,38 +138,4 @@ public class EightPuzzle {
             System.out.println();
         }
     }
-
-
-
 }
-
-class Node {
-    int[][] puzzle;
-    int x,y, cost, level;
-    Node parent;
-
-    public Node(int[][] puzzle, int x, int y, int newX,int level,Node parent, int newY){
-        // initialized the values
-        this.puzzle = new int[3][3];
-        this.x = newX;
-        this.y = newY;
-        this.cost = Integer.MAX_VALUE;
-        this.level = level;
-        this.parent = parent;
-    
-
-    for(int i =0; i< 3; i++){
-        for(int j = 0; j<3; j++){
-            // assign values 
-            this.puzzle[i][j] = puzzle[i][j];
-        }
-    }
-
-    // swapping the value of old node with the new values
-    int temp = this.puzzle[x][y];
-    this.puzzle[x][y] = this.puzzle[newX][newY];
-    this.puzzle[newX][newY] = temp;
-
-}
-}
-
